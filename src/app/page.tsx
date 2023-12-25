f3fa518c101df7d702754e58de29bc4426278cf3
John Aquino <johnaquino@outlook.com>
2023-12-25 09:12:44 -0500

    Add new files and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,49 @@
-import LeftColumn from "../components/LeftColumn"
-import RightColumn from "../components/RightColumn"
+"use client";
+
+import { useEffect, useState } from 'react';
+import LeftColumn from "../components/LeftColumn";
+import RightColumn from "../components/RightColumn";
+import ThemeToggle from "../components/ThemeToggle";
 
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    return localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  });
+  const [lightBgStyle, setLightBgStyle] = useState({ opacity: 1, transition: 'opacity 0.3s ease-in-out' });
+  const [darkBgStyle, setDarkBgStyle] = useState({ opacity: 0, transition: 'opacity 0.3s ease-in-out' });
+
+  useEffect(() => {
+    if (isDarkMode) {
+      setLightBgStyle({ ...lightBgStyle, opacity: 0 });
+      setTimeout(() => setDarkBgStyle({ ...darkBgStyle, opacity: 1 }), 150); // Start halfway through the light mode transition
+    } else {
+      setDarkBgStyle({ ...darkBgStyle, opacity: 0 });
+      setTimeout(() => setLightBgStyle({ ...lightBgStyle, opacity: 1 }), 150); // Start halfway through the dark mode transition
+    }
+
+    // Toggle the dark-mode class after a delay to sync with the opacity transition
+    setTimeout(() => {
+      document.body.classList.toggle('dark-mode', isDarkMode);
+      localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    }, 300);
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="container mx-auto p-4">
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <LeftColumn />
-      <RightColumn />
-    </div>
-    {/* <ExperienceSection /> */}
-    </div>
-  )
+    <>
+      <div className="bg-light-mode" style={lightBgStyle}></div>
+      <div className="bg-dark-mode" style={darkBgStyle}></div>
+      <ThemeToggle isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+      <div className="container mx-auto ps-16 pt-16 h-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      
+          <LeftColumn/>
+          <RightColumn />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from 'react';
+"use client";
+import { FaMoon, FaSun } from 'react-icons/fa';
 
-const ThemeToggle = () => {
-  const [theme, setTheme] = useState('light');
-
-  useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-    if (storedTheme) {
-      setTheme(storedTheme);
-      if (storedTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-      }
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-    document.body.classList.toggle('dark-mode');
-  };
-
+const ThemeToggle = ({ isDarkMode, toggleTheme }: { isDarkMode: boolean, toggleTheme: () => void }) => {
   return (
-    <button 
-      onClick={toggleTheme} 
-      className="fixed top-5 right-5 p-2 bg-gray-200 rounded hover:bg-gray-300"
-    >
-      Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+    <button onClick={toggleTheme} className="fixed top-5 right-5 p-2 bg-gray-200 text-xl rounded-full hover:bg-gray-300">
+      {isDarkMode ? <FaSun className="text-slate-500" /> : <FaMoon className="text-slate-500" />}
     </button>
   );
 };
